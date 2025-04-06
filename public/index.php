9cb0dfb8643acce5a727b034da0175db9acc792f
asmah <haddadasma006@gmail.com>
2025-04-06 22:46:55 +0200

    ajout du fichier index.php pour laffichage de la page principale

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php include '../includes/header.php'; ?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- la bage home  -->
+
+<h1>Welcome to Galerie Oselo Administration</h1>
+<p>Use the navigation to manage paintings and warehouses.</p>
+
+<?php include '../includes/footer.php'; ?>
